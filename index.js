d783fdce5e25cdfaff8554dc2b8fb96cd34a01e3
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-27 01:40:36 +0100

    feat: mongoose connection

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const home = require('./routes/home')
-const genres = require('./routes/genres')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const app = express();
 
-app.use(express.json())
-app.use('/', home)
-app.use('/api/genres', genres)
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB.."))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
 
-const port = process.env.PORT || 2000
-app.listen(port, () => console.log(`Listening on ${port}...`))
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+
+const port = process.env.PORT || 2000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
