749b805058e62afcafe54e179e72c454bc36d713
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-27 01:38:44 +0100

    feat: db mobels

---
+++ models/customer.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const customerScehma = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
+  phone: { type: String, required: true, minlength: 5, maxlength: 50 },
+  isGold: { type: Boolean, default: false },
+});
+
+const Customer = mongoose.model("Customer", customerScehma);
+
+function validateCustomer(customer) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean(),
+  });
+
+  return schema.validate(customer);
+}
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
