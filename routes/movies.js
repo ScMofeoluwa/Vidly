85117068928b1bdb653556ea1b27ece5af142099
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-29 23:24:34 +0100

    feat: movies and rentals routes

---
+++ routes/movies.js
@@ -0,0 +1,63 @@
+const { Movie, validate } = require("../models/movie");
+const { Genre } = require("../models/genre");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("title");
+  res.send(movies);
+});
+
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
+  if (!movie) return res.status(404).send("Movie with the given ID does not exist");
+  return res.send(movie);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(404).send("Invalid Genre");
+
+  let movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  movie = await movie.save();
+  res.send(movie);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(404).send("Invalid Genre");
+
+  const movie = await Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  if (!movie) return res.status(404).send("Movie with given ID does not exist");
+  return res.send(movie);
+});
+
+router.delete("/:id", async (req, res) => {
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+  if (!movie) return res.status(400).send("Movie with given ID does not exist");
+  res.send(movie);
+});
+
+module.exports = router;
---
+++ routes/rentals.js
@@ -0,0 +1,45 @@
+const { Rental, validate } = require("../models/rental");
+const { Movie } = require("../models/movie");
+const { Customer } = require("../models/customer");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const rentals = Rental.find().sort("-dateOut");
+  res.send(rentals);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer)
+    return res.status(404).send("Customer with given ID does not exist");
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(404).send("Movie with given ID does not exist");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock");
+
+  let rental = new Rental({
+    customer: {
+      id_: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+      isGold: customer.isGold,
+    },
+    movie: {
+      id_: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
+  });
+  rental = await rental.save(rental);
+  movie.numberInStock--;
+  movie.save();
+  return res.send(rental);
+});
+
+module.exports = router;
