8b5008d721fcf90643b7cbbd76f1c9693a7f16ca
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2021-11-29 23:27:35 +0100

    chore: lezzgedizz

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const app = express();
 
 mongoose
@@ -12,6 +14,8 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 2000;
 app.listen(port, () => console.log(`Listening on ${port}...`));
